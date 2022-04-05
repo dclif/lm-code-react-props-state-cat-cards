9f46ccc4278219989c3d05e552111039abea1570
Daniel <65896341+dclif@users.noreply.github.com>
2022-04-05 19:31:29 +0100

    activity 5

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Navbar from './components/navbar';
 import Header from './components/header';
 import Footer from './components/footer';
 import Cat from './data/cat'
+import CatCard from './components/cat_card'
 
 function App() {
 	const [cats, setCats] = useState<Array<Cat>>(
@@ -89,7 +90,15 @@ function App() {
 			<Header />
 
 			<main>
-				<div className='cards__wrapper'>{/* JSX code can go here */}</div>
+				<div className='cards__wrapper'>
+					{cats.map(cat => <CatCard
+					name={cat.name}
+					species={cat.species}
+					favFoods={cat.favFoods}
+					birthYear={cat.birthYear}
+					/>)}
+					
+				</div>
 			</main>
 
 			<Footer />
---
+++ src/components/cat_card.tsx
@@ -0,0 +1,20 @@
+interface CatCardProps{
+    name: string;
+    species: string;
+    favFoods: Array<string>;
+    birthYear: number;
+ }
+
+const CatCard : React.FC<CatCardProps> = (props) => 
+{
+    console.log(props)
+    return(
+<div className="card">
+<h3 className="card__text card__header">{props.name}</h3>
+<p className="card__text">Species: {props.species}</p>
+<p className="card__text">Favourite Food(s):{props.favFoods}</p>
+<p className="card__text">Birth year:{props.birthYear}</p>
+</div>
+    )
+}
+export default CatCard;
