5b91f88d2c334ef790aa0e102a5e2a719d8d8e30
Daniel <65896341+dclif@users.noreply.github.com>
2022-04-06 09:40:56 +0100

    activity 6

--- src/App.tsx
+++ src/App.tsx
@@ -91,12 +91,15 @@ function App() {
 
 			<main>
 				<div className='cards__wrapper'>
-					{cats.map(cat => <CatCard
-					name={cat.name}
-					species={cat.species}
-					favFoods={cat.favFoods}
-					birthYear={cat.birthYear}
-					/>)}
+				{cats.map((cat, index) => (
+						<CatCard
+							name={cat.name}
+							species={cat.species}
+							favFoods={cat.favFoods}
+							birthYear={cat.birthYear}
+							catIndex={index}
+						/>
+					))}
 					
 				</div>
 			</main>
